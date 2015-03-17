873a97ee1b4fe4af258775afe636015d2c5c8b5c
François Lafont <flafdivers@free.fr>
2015-03-17 23:48:27 +0100

    Create a new script for the building

---
+++ build/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+script_dir=$(cd $(dirname "$0"); pwd)
+pkg_name="se3-clients-linux"
+
+cd "$script_dir" || {
+    echo "Error, impossible change directory to $script_dir."
+    echo "End of the script."
+    exit 1
+}
+
+# Remove old *.deb files.
+rm -rf "$script_dir/"*.deb
+
+cp -ra "$script_dir/../src" "$script_dir/$pkg_name"
+dpkg --build "$script_dir/$pkg_name" .
+
+# Cleaning.
+rm -r "$script_dir/$pkg_name"
+
