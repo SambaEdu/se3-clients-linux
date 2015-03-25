0c48871418c40e8885c7baa9471525f321d03a0a
naldegheri <naldegheri@yahoo.fr>
2015-03-25 10:35:33 +0100

    Corrections de quelques bugs pour Trusty et Jessie

--- src/home/netlogon/clients-linux/ltsp/installation_LTSP_1carte_wheezy.sh
+++ src/home/netlogon/clients-linux/ltsp/installation_LTSP_1carte_wheezy.sh
@@ -10,13 +10,18 @@ IP_SE3="$ip_se3"
 IP_LTSP=`ifconfig eth0 | grep 'inet adr:' | cut -d: -f2 | awk '{ print $1}'`
 
 HOTE=`cat /etc/hostname`
-VENDOR="Debian"								# On construit au moins un environnement correspondant à celui du serveur LTSP
-CONFIG_NBD="false"							# On utilise plutôt NFS sous Debian
-DISTRIB="wheezy"			
-ARCHI="i386"
-DESKTOP="lxde"
-#MIRROIR="http://ftp.fr.debian.org/debian/"
-MIRROIR="http://$IP_SE3:9999/debian/"
+export VENDOR="Debian"			# Debian ou Ubuntu
+export CONFIG_NBD="false"		# mettre à true pour Ubuntu
+DISTRIB="wheezy"				# wheezy ou jessie ou trusty
+ARCHI="i386"					# i386 ou amd64
+DESKTOP="lxde"					# lxde ou xfce4 ou gnome ou ubuntu-desktop
+
+if [ "$VENDOR" = "Debian" ]; then
+	MIRROIR="http://$IP_SE3:9999/debian/"
+else
+	MIRROIR="http://$IP_SE3:9999/ubuntu/"
+fi
+
 ENVIRONNEMENT="$DISTRIB-$DESKTOP-$ARCHI"
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
@@ -30,9 +35,22 @@ if [ "$REPONSE" != "o" ]; then
 	exit 0;
 fi
 
+
 ########################################################################
 # Quelques vérifications pour le bon déroulement de l'installation ...
 ########################################################################
+
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " Votre serveur LTSP doit avoir une adresse IP statique																		"
+echo " L'est-elle ? ou avez-vous réservé son adresse IP dans le module dhcp du se3 ? o ou n ? 										"
+read REPONSE
+if [ "$REPONSE" != "o" ]; then
+	echo "Installation interrompue"
+	echo " Réserver, via le module dhcp de l'interface web du se3, l'addresse IP de votre (futur) serveur LTSP 						"
+	read FIN
+	exit 0;
+fi
+echo "------------------------------------------------------------------------------------------------------------------------------"
 
 LOGIN=$(who i am | cut -d ' ' -f1)
 if [ "$LOGIN" != "admin" ]; then
@@ -54,7 +72,7 @@ fi
 if [ ! -s "$REP_LTSP/addr_MAC_clients" ]; then
 	echo "Installation interrompue"
 	echo "Le fichier addr_MAC_client contenant la description des adresses MAC de vos clients légers est vide"
-	echo "Veuillez le renseigner en mettant une addresse MAC par ligne"
+	echo "Veuillez le renseigner en mettant une addresse MAC par ligne et par client léger"
 	echo "Pour récupérer rapidement l'adresse MAC de vos clients légers, vous pouvez les démarrer puis consulter le module dhcp (Gestion des baux) de l'interfac Web du SE3"
 	echo "N'oubliez pas (si c'est le cas) de supprimer vos futures clients légers de la réservation active du dhcp du se3 (le dhcp du SE3 risque sinon de ne pas redémarrer ...)"
 	exit 1;
@@ -69,8 +87,11 @@ apt-get update
 apt-get install -y ltsp-server ldm-server ltsp-docs
 
 mkdir /opt/ltsp
-echo '/opt/ltsp *(ro,no_root_squash,async,no_subtree_check)' > /etc/exports			# Configuration du serveur NFS (faire un "man exports" pour les options)
-service nfs-kernel-server restart
+
+if [ "$CONFIG_NBD" = "false" ]; then
+	echo '/opt/ltsp *(ro,no_root_squash,async,no_subtree_check)' > /etc/exports			# Configuration du serveur NFS (faire un "man exports" pour les options)
+	service nfs-kernel-server restart
+fi
 
 ln -s /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/trusty
 	
@@ -78,25 +99,28 @@ ln -s /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/trusty
 echo '                    Fin de l installation du serveur d environnement de d applications       '
 echo "---------------------------------------------------------------------------------------------"
 
-echo "---------------------------------------------------------------------------------------------"
-echo "         Construction de l environnement Debian Wheezy LXDE des clients légers               "
+echo "---------------------------------------------------------------------------------------------------------------------------------"
+echo " Voulez-vous construire un environnement $VENDOR $DISTRIB $DESKTOP $ARCHI pour vos clients légers avec NBD à $CONFIG_NBD ? "
+echo " o ou n ?"
+read REPONSE
+if [ "$REPONSE" != "o" ]; then
+	echo "Installation interrompue"
+	read FIN
+	exit 0;
+fi
+
 echo " "
-echo " INFORMATIONS : 											                 								 "
-echo " Par défaut, les clients légers sont configurés en fat client afin d alleger le serveur LTSP				 "
-echo " Cela suppose que vos clients légers ont au moins 1 Go de RAM pour un usage correct avec le bureau LXDE    "
-echo " Si RAM < 1Go, il est possible de désactiver le mode fatclient dans le fichier lts.conf du serveur LTSP    "
+echo " INFORMATIONS : 											                 								 			 "
+echo " Par défaut, les clients légers sont configurés en fat client afin d alleger le serveur LTSP				 			 "
+echo " Cela suppose que vos clients légers ont au moins 1 Go de RAM pour un usage correct avec un bureau léger comme LXDE    "
+echo " Si RAM < 1Go, il est possible, après l'installation, de désactiver le mode fatclient dans le fichier lts.conf   		 "
 		
 ltsp-build-client --arch "$ARCHI" --dist "$DISTRIB" --chroot "$ENVIRONNEMENT" --fat-client-desktop "$DESKTOP" --mirror "$MIRROIR" --purge-chroot
 
-if [ "$DISTRIB" = "jessie" ]; then
+# Pour compatibilité avec Wheezy qui utilise le fichier default pour son boot PXE
+if [ "$DISTRIB" = "jessie" ] || [ "$DISTRIB" = "trusty" ]; then
 	cp "/srv/tftp/ltsp/$ENVIRONNEMENT/pxelinux.cfg/ltsp" "/srv/tftp/ltsp/$ENVIRONNEMENT/pxelinux.cfg/default"
 fi
-
-if [ "$DISTRIB" = "trusty" ]; then
-	cp "/srv/tftp/ltsp/$ENVIRONNEMENT/pxelinux.cfg/ltsp" "/srv/tftp/ltsp/$ENVIRONNEMENT/pxelinux.cfg/default"
-	service nbd-server restart
-fi
-
 
 echo "         Fin de la construction de l environnement Debian Wheezy LXDE des clients légers     "
 echo "---------------------------------------------------------------------------------------------"
@@ -112,7 +136,7 @@ echo '                    Intégration du serveur LTSP au se3     						        
 if [ -e /etc/pam.d/sshd_save ]; then
 	cp -f /etc/pam.d/sshd_save /etc/pam.d/sshd			# Il s'agit d'une réinstallation
 else
-	cp -f /etc/pam.d/sshd /etc/pam.d/sshd_save			# Sauvegarde avant modification
+	cp /etc/pam.d/sshd /etc/pam.d/sshd_save				# Sauvegarde avant modification
 fi
 
 sed -i -e 's/^@include.*/&.AVEC-LDAP/' /etc/pam.d/sshd																			# Identification avec l'annuaire LDAP du se3
@@ -143,25 +167,24 @@ chmod 700 /etc/se3/bin/ouverture_ltsp.sh			# Mettre les droits en cohérence ave
 
 cat <<EOF > "/opt/ltsp/$ENVIRONNEMENT/etc/lts.conf"
 # Pour obtenir une liste détaillée ainsi qu'une explication des paramètres de ce fichier, saisir sur le serveur LTSP : man lts.conf
-[default]					# Paramètres par défaults
+[default]				      # Paramètres par défaults
 LTSP_CONFIG=True			
+LTSP_FATCLIENT=True			  # Par défault tous les clients légers sont paramétrés en fat client
+LOCAL_APPS_EXTRAMOUNTS=/mnt   # Montage du dossier /mnt du serveur LTSP dans l environnement du fat client afin d obtenir les partages Samba
 
 LDM_LANGUAGE=fr_FR.UTF-8
-LDM_DIRECTX=True
+LDM_DIRECTX=True			  # Par défaut, on ne cripte pas les échanges graphiques, pour plus de fluidité
 XKBLAYOUT=fr
 X_NUMLOCK=True
 
-LTSP_FATCLIENT=True							  # Par défault tous les clients légers sont paramétrés en fat client
-LOCAL_APPS_EXTRAMOUNTS=/mnt                   # Montage du dossier /mnt dans l environnement du fat client afin d obtenir les partages Samba
-
-[leger]										  # Configuration des clients légers
+[leger]					      # Configuration en client léger
 LTSP_FATCLIENT=False
 
-[hybrid]									  # Configuration des clients légers hybride
+[hybrid]			 	      # Configuration en client hybride
 LTSP_FATCLIENT=False
-LOCAL_APPS=True                               # Activie le mode hybride 
-LOCAL_APPS_MENU=True                          # Autoriser les applications disponibles dans la liste d applications du bureau à s exécuter sur le client léger
-LOCAL_APPS_MENU_ITEMS=iceweasel	 	  		  # Lister les applications .desktop qui doivent être exécutées avec les ressources du client léger (ces applications doivent au préalable être chrootée )
+LOCAL_APPS=True                       # Activer le mode hybride 
+LOCAL_APPS_MENU=True                  # Autoriser les applications disponibles dans la liste d applications du bureau du serveur LTSP à s exécuter avec les ressources (RAM et Processeur) du client léger
+LOCAL_APPS_MENU_ITEMS=vlc,gimp	 	  # Liste des applications .desktop qui doivent être exécutées avec les ressources du client léger (ces applications doivent au préalable être chrootée dans l'environnement du client léger ...)
 
 EOF
 
@@ -175,15 +198,14 @@ group {					               # description des clients légers qui utilisent l'env
        next-server $IP_LTSP;                        	  # IP du serveur TFTP (c'est à dire du serveur LTSP)
        filename "/ltsp/$ENVIRONNEMENT/pxelinux.0";        # chemin du chargeur d'amorçage PXE
         
-         # On définit chaque client léger désservi par le serveur LTSP en indiquant son nom d'hôte, son adresse mac et ip
-         # Client léger N°1
-         #       host clientleger1 {                    # nom d'hôte ou dns du client
-         #       hardware ethernet 00:fc:b9:15:13:gh;   # adresse MAC du client 1
-         #       fixed-address 172.20.100.1;            # adresse IP du client (à choisir en fonction de la plage d'adresse du serveur DHCP, le se3)
-         #       }
-         
        }
 EOF
+
+
+########################################################################################################################################################
+# Récupération manuelle de quelques paramètres du fichier dhcp du se3
+# Nécessaire pour faire "cohabiter" les clients légers avec les autres clients du réseau se3	
+########################################################################################################################################################
 
 echo "Ouvrir un navigateur Web puis, via l'interface Web du se3, consulter le menu Configuration du module dhcp"
 echo 'Entrée l adrese IP de la "Fin de la plage dynamique"'
@@ -199,12 +221,22 @@ IP_CL_FIN1=$(echo $IP_CL_FIN | cut -d'.' --fields="1 2 3")
 IP_CL_FIN2=$(echo $IP_CL_FIN | cut -d'.' -f4)
 	
 if [ $IP_CL_DEB1 != $IP_CL_FIN1 ]; then
-	echo 'Il y a un problème entre l adresse IP de fin de plage active et de début de plage de réservation'
+	echo 'Il y a un problème entre l adresse IP de fin de plage active et celle de début de plage de réservation'
 	exit 1
 fi
 	
 NB_CL=$(($IP_CL_FIN2 - $IP_CL_DEB2 - 1)) 
-echo "Vous pouvez saisir au maximum $NB_CL clients légers dans le dhcp de votre se3"
+
+echo " Vous pouvez intégrer au maximum $NB_CL clients légers dans le dhcp de votre se3"
+echo " Vous pouvez augmenter ce nombre soit : "
+echo " 		- en diminuant l'adresse de Fin de la plage dynamique dans le module dhcp du se3"
+echo " 		- en augmentant l'adresse de Début de la plage de reservation dans le module dhcp du se3"
+echo " Voulez-vous poursuivez l'installation ? o ou n ? "
+read REPONSE
+if [ "$REPONSE" != "o" ]; then
+	echo "Installation interrompue"
+	exit 0;
+fi
 
 i=1
 for MAC in $(cat addr_MAC_clients)
@@ -212,7 +244,7 @@ do
 
 	IP_CL="$IP_CL_DEB1.$(($IP_CL_DEB2 + $i))"				# Adresse IP du client léger
 	
-	sed -i -e '$d' "$REP_LTSP/dhcpd_ltsp.conf"				# On supprime l'accolade de fin de group 
+	sed -i -e '$d' "$REP_LTSP/dhcpd_ltsp.conf"				# On supprime l'accolade de fin du group 
 	
 cat <<EOF >> "$REP_LTSP/dhcpd_ltsp.conf"
 	host fatclient$i { 
@@ -237,6 +269,9 @@ echo "            Installation des applications de base dans l'environnement des
 # Modification des sources des depots de l'environnement des fat client
 ########################################################################################################################################################
 
+
+if [ "$VENDOR" = "Debian" ]; then
+
 cat <<EOF > "/opt/ltsp/$ENVIRONNEMENT/etc/apt/sources.list"
 
 deb http://$IP_SE3:9999/debian $DISTRIB main non-free contrib
@@ -253,13 +288,21 @@ deb-src http://$IP_SE3:9999/debian $DISTRIB-backports main non-free contrib
 
 EOF
 
+fi
+
 ########################################################################################################################################################
 # Installation des applis de base (pouvant être installé avec un apt-get)
 ########################################################################################################################################################
 ltsp-chroot --arch "$ENVIRONNEMENT" apt-get update				
 ltsp-chroot --arch "$ENVIRONNEMENT" apt-get -y install numlockx aptitude								
 
-for PAQUET in $(cat "$REP_LTSP/mesapplis-debian.txt")
+if [ "$VENDOR" = "Ubuntu" ]; then
+	MESAPPLIS="mesapplis-ubuntu.txt"
+else
+	MESAPPLIS="mesapplis-debian.txt"
+fi
+
+for PAQUET in $(cat "$REP_LTSP/$MESAPPLIS")
 do
 	#installation des paquets
 	echo -e "=========================="
@@ -271,28 +314,28 @@ do
 	
 done
 
-#ltsp-chroot --arch "$ENVIRONNEMENT" aptitude -y --full-resolver install `cat "$REP_LTSP/mesapplis-debian.txt"`	# Applications de base de Debian
+#ltsp-chroot -m --arch "$ENVIRONNEMENT" aptitude -y --full-resolver install `cat "$REP_LTSP/$MESAPPLIS"`	# Applications de base de Debian
 
 ########################################################################################################################################################
 # Installation d'Epoptes Maître sur le serveur LTSP
 ########################################################################################################################################################
-apt-get update
 apt-get install -y epoptes				# On installe l'application maître sur le serveur LTSP
-
-echo "Voulez-vous ajouter un utilisateur (un enseignant) au groupe epoptes afin qu'il puisse utiliser cette application ? o ou n ?"
-read REPONSE
-if [ "$REPONSE" = "o" ]; then
-	echo "Saisir le nom de cet utilisateur (son identifiant) : "
-	read UTILISATEUR
-	gpasswd -a "$UTILISATEUR" epoptes
-fi
+gpasswd -a admin epoptes				# On ajoute le compte admin comme utilisateur d'epoptes maître
 
 ########################################################################################################################################################
 # Installation d'Epoptes client dans l'environnement des fat clients
 ########################################################################################################################################################
 ltsp-chroot --arch "$ENVIRONNEMENT" aptitude install -y epoptes-client
-sed -i -e "s/^#SERVER.*/SERVER=$IP_LTSP/g" "/opt/ltsp/$ENVIRONNEMENT/etc/default/epoptes-client"		# On indique IP d'epoptes maître (c'est à dire celle du serveur LTSP)
+sed -i -e "s/^#SERVER.*/SERVER=$IP_LTSP/g" "/opt/ltsp/$ENVIRONNEMENT/etc/default/epoptes-client"		# On indique l'IP d'epoptes maître (c'est à dire celle du serveur LTSP)
 ltsp-chroot --arch "$ENVIRONNEMENT" epoptes-client -c													# On récupère le certificat du maître
+
+########################################################################################################################################################
+# Dans le cas de l'utilisation de NBD à la place de NFS (sous Ubuntu par exemple), on reconstruit l'image compressée ... (compter un quart d'heure)
+########################################################################################################################################################
+if [ "$CONFIG_NBD" = "true" ]; then
+	ltsp-update-image "$ENVIRONNEMENT"
+	service nbd-server restart
+fi
 
 echo "            Fin de l'installation des applis dans l'environnement des fat clients 	   	   "
 echo "---------------------------------------------------------------------------------------------"
@@ -306,6 +349,5 @@ echo " ln -s /home/netlogon/clients-linux/ltsp/dhcpd_ltsp.conf /etc/dhcp/dhcpd_l
 echo " Puis, via l'interface web du se3, dans le menu configuration du module DHCP, renseigner le fichier de conf à inclure suivant : "
 echo " /etc/dhcp/dhcpd_ltsp.conf "
 echo " Enfin, valider la modification (le serveur DHCP va redémarrer et inclure ce fichier de conf) "
-echo " Démarrer vos clients légers et vérifier que tout fonctionne; "
+echo " Démarrer vos clients légers et vérifier que tout fonctionne "
 echo "------------------------------------------------------------------------------------------------------------------"
-read FIN
