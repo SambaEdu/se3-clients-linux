f386ef9cb04ef14cfbf0f789b18b73f31e2b35ec
lafont <francois.lafont@ac-versailles.fr>
2016-05-10 11:35:29 +0200

    Cosmetic: set indentation

--- src/home/netlogon/clients-linux/lib.sh
+++ src/home/netlogon/clients-linux/lib.sh
@@ -16,35 +16,32 @@
 
 download_open_sankore_deb()
 {
+    local repertoire_install="$1"
+    local url_open_sankore='http://www.cndp.fr/open-sankore/OpenSankore/Releases/v2.5.1'
+    local version_open_sankore='Open-Sankore_Ubuntu_12.04_2.5.1'
 
-local repertoire_install="$1"
-local url_open_sankore='http://www.cndp.fr/open-sankore/OpenSankore/Releases/v2.5.1'
-local version_open_sankore='Open-Sankore_Ubuntu_12.04_2.5.1'
+    if [ ! -d "/home/netlogon/clients-linux/divers/open-sankore" ]
+    then
+        mkdir -p /home/netlogon/clients-linux/divers/open-sankore
+    fi
 
-if [ ! -d "/home/netlogon/clients-linux/divers/open-sankore" ]
-then
-	mkdir -p /home/netlogon/clients-linux/divers/open-sankore
-fi
+    if [ ! -e "/home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_amd64.zip" ] 
+    then
+        wget "${url_open_sankore}/${version_open_sankore}_amd64.zip"
+        chmod 755 ${version_open_sankore}_amd64.zip
+        mv ${version_open_sankore}_amd64.zip "/home/netlogon/clients-linux/divers/open-sankore"
+    fi
 
-if [ ! -e "/home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_amd64.zip" ] 
-then 
-	wget "${url_open_sankore}/${version_open_sankore}_amd64.zip"
-	chmod 755 ${version_open_sankore}_amd64.zip
-	mv ${version_open_sankore}_amd64.zip "/home/netlogon/clients-linux/divers/open-sankore"
-fi
+    if [ ! -e "/home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_i386.zip" ]
+    then
+        wget "${url_open_sankore}/${version_open_sankore}_i386.zip"
+        chmod 755 ${version_open_sankore}_i386.zip
+        mv ${version_open_sankore}_i386.zip "/home/netlogon/clients-linux/divers/open-sankore"
+    fi
 
+    cp /home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_*.zip "$repertoire_install"
 
-if [ ! -e "/home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_i386.zip" ]
-then 
-	wget "${url_open_sankore}/${version_open_sankore}_i386.zip"
-	chmod 755 ${version_open_sankore}_i386.zip
-	mv ${version_open_sankore}_i386.zip "/home/netlogon/clients-linux/divers/open-sankore"
-fi
-
-cp /home/netlogon/clients-linux/divers/open-sankore/${version_open_sankore}_*.zip "$repertoire_install"
-
-return 0
-
+    return 0
 }
 
 
@@ -58,42 +55,40 @@ return 0
 
 install_open_sankore()
 {
-	
-local compte_rendu="$1"
-local IP_SE3="$2"
-local test_arch="$(arch)"
-local version_open_sankore='Open-Sankore_Ubuntu_12.04_2.5.1'
+    local compte_rendu="$1"
+    local IP_SE3="$2"
+    local test_arch="$(arch)"
+    local version_open_sankore='Open-Sankore_Ubuntu_12.04_2.5.1'
 
-echo "Installation d'Open-Sankore" | tee -a $compte_rendu
+    echo "Installation d'Open-Sankore" | tee -a $compte_rendu
 
+    if [ "$test_arch" = "x86_64" ]
+    then
+        wget -q "http://${IP_SE3}/install/${version_open_sankore}_amd64.zip"
+        if [ "$?" = "0" ] 
+        then
+            mkdir open-sankore
+            unzip -d open-sankore "${version_open_sankore}_amd64.zip"
+            dpkg -i open-sankore/Open-Sankore*_amd64.deb > /dev/null
+            apt-get install -f
+            rm -rf Open-Sankore_Ubuntu*.zip open-sankore
+        fi
+    fi
 
-if [ "$test_arch" = "x86_64" ]
-then
-	wget -q "http://${IP_SE3}/install/${version_open_sankore}_amd64.zip"
-	if [ "$?" = "0" ] 
-	then
-		mkdir open-sankore
-		unzip -d open-sankore "${version_open_sankore}_amd64.zip"
-		dpkg -i open-sankore/Open-Sankore*_amd64.deb > /dev/null
-		apt-get install -f 
-		rm -rf Open-Sankore_Ubuntu*.zip open-sankore
-	fi
-fi
+    if [ "$test_arch" = "i686" ]
+    then
+        wget -q "http://${IP_SE3}/install/${version_open_sankore}_i386.zip"
+        if [ "$?" = "0" ]
+        then
+            mkdir open-sankore
+            unzip -d open-sankore "${version_open_sankore}_i386.zip"
+            dpkg -i open-sankore/Open-Sankore*_i386.deb > /dev/null
+            apt-get install -f
+            rm -rf Open-Sankore_Ubuntu*.zip open-sankore
+        fi
+    fi
 
-if [ "$test_arch" = "i686" ]
-then
-	wget -q "http://${IP_SE3}/install/${version_open_sankore}_i386.zip"
-	if [ "$?" = "0" ] 
-	then
-		mkdir open-sankore
-		unzip -d open-sankore "${version_open_sankore}_i386.zip"
-		dpkg -i open-sankore/Open-Sankore*_i386.deb > /dev/null
-		apt-get install -f 
-		rm -rf Open-Sankore_Ubuntu*.zip open-sankore
-	fi
-fi
-
-return 0
+    return 0
 
 }
 
