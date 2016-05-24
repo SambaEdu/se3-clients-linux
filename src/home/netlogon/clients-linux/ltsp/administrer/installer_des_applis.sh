3925eb59fe724ee5ea577351c269c34bdafd5d5b
naldegheri <nicolas.aldegheri@ac-versailles.fr>
2016-05-24 21:01:33 +0200

    correct install_mes_applis.sh

--- src/home/netlogon/clients-linux/ltsp/administrer/installer_des_applis.sh
+++ src/home/netlogon/clients-linux/ltsp/administrer/installer_des_applis.sh
@@ -3,9 +3,4 @@
 SE3="__SE3__"
 ENVIRONNEMENT="i386"													# Nom du chroot du client lourd
 
-xterm -e "echo 'Veuillez saisir la liste d applications, séparées chacune par un espace'; read LISTE_APPLIS; ssh -o 'StrictHostKeyChecking no' -l root '$SE3' bash << EOF
-
-ltsp-chroot -m --arch "$ENVIRONNEMENT" apt-get install -y -f "$LISTE_APPLIS_APT_GET"
-sleep 3
-exit 0
-EOF"
+xterm -e "printf 'Veuillez saisir la liste d applications, séparées chacune par un espace \n'; read LISTE_APPLIS; ssh -o 'StrictHostKeyChecking no' -l root '$SE3' \"ltsp-chroot -m --arch "$ENVIRONNEMENT" apt-get install -y -f \"\$LISTE_APPLIS\"\"; printf 'Installations terminées'; sleep 5"
