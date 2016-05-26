216156b3b4a8d05cce58d7daa279524a3f97da57
naldegheri <nicolas.aldegheri@ac-versailles.fr>
2016-05-26 20:54:50 +0200

    debug dployer_imprimantes.sh script

--- src/home/netlogon/clients-linux/ltsp/administrer/deployer_imprimantes.sh
+++ src/home/netlogon/clients-linux/ltsp/administrer/deployer_imprimantes.sh
@@ -6,12 +6,12 @@ CHEMIN_CUPS='/home/admin/Clients-linux/ltsp/cups'
 
 rm -rf --one-file-system "$CHEMIN_CUPS"
 
-xterm -e "printf 'Saisir le mot de passe du compte root des clients lourds \n'; su - root -c 'cp -a /etc/cups "$CHEMIN_CUPS"; sleep 3"
+xterm -e "printf 'Saisir le mot de passe du compte root des clients lourds \n'; su - root -c 'cp -r /etc/cups "$CHEMIN_CUPS"; sleep 3'"
 
-xterm -e "printf 'Saisir le mot de passe du compte root du se3 \n'; ssh -o 'StrictHostKeyChecking no' -l root '$SE3' bash << 'EOF'
+xterm -e "printf 'Vérifier que le partage Clients-linux/ltsp/cups n'est pas vide \n'; printf 'Puis saisir le mot de passe du compte root du se3 \n'; ssh -o 'StrictHostKeyChecking no' -l root '$SE3' bash << 'EOF'
 
 rm -rf "/opt/ltsp/$ENVIRONNEMENT/etc/cups"
-cp -a "$CHEMIN_CUPS" "/opt/ltsp/$ENVIRONNEMENT/etc/"
+cp -r "/home/netlogon/clients-linux/ltsp/cups" "/opt/ltsp/$ENVIRONNEMENT/etc/"
 
 sleep 3
 exit 0
