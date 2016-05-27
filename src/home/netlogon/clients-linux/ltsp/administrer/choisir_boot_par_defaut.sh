533fdbbdef8d325bd479e8c504e6e9496b21d3fe
naldegheri <nicolas.aldegheri@ac-versailles.fr>
2016-05-27 20:55:20 +0200

    script to choose default PXE boot on time out : with hard disk or as fat client

---
+++ src/home/netlogon/clients-linux/ltsp/administrer/choisir_boot_par_defaut.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SE3="__SE3__"
+ENVIRONNEMENT="i386"													# Nom du chroot du client lourd
+
+xterm -e "printf 'Taper 1 pour faire booter tous les PC PXE du réseau sur leur disque dur \n'; printf 'Taper 2 pour faire booter tous les PC PXE du réseau en client lourd \n'; read REPONSE; ssh -o 'StrictHostKeyChecking no' -l root '$SE3' bash << EOF
+
+if [ \"\$REPONSE\" = 2 ]
+then
+	sed -i -e 's/^ONTIMEOUT*/ONTIMEOUT ltsp/g' /tftpboot/pxelinux.cfg/default		
+	printf 'Tous les PC PXE du réseau démarreront par défaut en client lourd ltsp'
+	sleep 5
+else
+	sed -i -e 's/^ONTIMEOUT*/ONTIMEOUT bootlocal/g' /tftpboot/pxelinux.cfg/default		
+	printf 'Tous les PC PXE du réseau démarreront par défaut avec leur disque dur'
+	sleep 5
+fi
+
+exit 0
+
+EOF"
