63feca80a92accfe6c42b29983da44d3d19458b3
naldegheri <nicolas.aldegheri@ac-versailles.fr>
2016-05-28 14:11:26 +0200

    debug choisir_boot_par_defaut.sh

--- src/home/netlogon/clients-linux/ltsp/administrer/choisir_boot_par_defaut.sh
+++ src/home/netlogon/clients-linux/ltsp/administrer/choisir_boot_par_defaut.sh
@@ -7,11 +7,11 @@ xterm -e "printf 'Taper 1 pour faire booter tous les PC PXE du réseau sur leur 
 
 if [ \"\$REPONSE\" = 2 ]
 then
-	sed -i -e 's/^ONTIMEOUT*/ONTIMEOUT ltsp/g' /tftpboot/pxelinux.cfg/default		
+	sed -i -e 's/^ONTIMEOUT.*/ONTIMEOUT ltsp/g' /tftpboot/pxelinux.cfg/default		
 	printf 'Tous les PC PXE du réseau démarreront par défaut en client lourd ltsp'
 	sleep 5
 else
-	sed -i -e 's/^ONTIMEOUT*/ONTIMEOUT bootlocal/g' /tftpboot/pxelinux.cfg/default		
+	sed -i -e 's/^ONTIMEOUT.*/ONTIMEOUT bootlocal/g' /tftpboot/pxelinux.cfg/default		
 	printf 'Tous les PC PXE du réseau démarreront par défaut avec leur disque dur'
 	sleep 5
 fi
